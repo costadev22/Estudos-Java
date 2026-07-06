0f96bf4fafe30208a8b3093ac2791ebde77b2129
Washington Costa <costasantana980@gmail.com>
2026-07-06 19:50:42 -0300

    Método Static

---
+++ Java-Curso/src/MetodoStatic/App.java
@@ -0,0 +1,10 @@
+package MetodoStatic;
+
+public class App {
+    static void main(String[] args) {
+        // Modificador static
+        System.out.println(Matematica.calcularAreaCirculo(25));
+
+
+    }
+}
---
+++ Java-Curso/src/MetodoStatic/Jogo.java
@@ -0,0 +1,7 @@
+package MetodoStatic;
+
+public class Jogo {
+    public void jogar(){
+        double areaJogador = Matematica.calcularAreaCirculo(4);
+    }
+}
